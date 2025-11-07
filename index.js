d6f1fe77f3671aa9215da8060bdb49ccda2626f2
IsBenben <162774387+IsBenben@users.noreply.github.com>
2025-11-07 18:32:58 +0800

    背景canvas，找规律项目

--- index.js
+++ index.js
@@ -17,17 +17,48 @@ svg.style.display = 'none';
 document.body.appendChild(svg);
 setTimeout(() => {
   svg.remove();
-}, 800);
+}, 1300);
 
 function choice(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-const backgroundContainer = document.querySelector('.background');
-backgroundContainer.innerHTML = Array.from(
-  { length: 6 },
-  () => '<p>' + Array.from({ length: 35 }, () => choice('01')).join('') + '</p>'
-).join('');
+const background = document.createElement('canvas');
+background.className = 'background';
+const ctx = background.getContext('2d');
+function initCanvas() {
+  background.width = window.innerWidth * devicePixelRatio;
+  background.height = window.innerHeight * devicePixelRatio;
+}
+initCanvas();
+window.addEventListener('resize', () => {
+  initCanvas();
+});
+ctx.textBaseline = 'top';
+const charIndex = [];
+setInterval(() => {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+  ctx.fillRect(0, 0, background.width, background.height);
+  ctx.fillStyle = 'hsl(110, 50%, 60%)';
+  const fontsize = 16 * devicePixelRatio;
+  ctx.font = `${fontsize}px 'JetBrains Mono', sans-serif`;
+  const columns = Math.ceil(background.width / fontsize);
+  for (let i = 0; i < columns - charIndex.length; i++) {
+    charIndex.push(0);
+  }
+  for (let i = 0; i < charIndex.length; i++) {
+    const char = choice('1234567890ABCDEF');
+    const y = charIndex[i] * fontsize;
+    ctx.fillText(char, i * fontsize, y);
+    charIndex[i]++;
+    if (y > background.height) {
+      if (Math.random() < 0.01) {
+        charIndex[i] = 0;
+      }
+    }
+  }
+}, 20);
+document.body.insertBefore(background, document.body.firstChild);
 
 const cards = document.querySelectorAll('.card');
 window.addEventListener('mousemove', (e) => {
@@ -96,6 +127,11 @@ for (const code of codes) {
   code.addEventListener('click', () => {
     navigator.clipboard.writeText(code.textContent);
   });
+}
+
+const pres = document.querySelectorAll('pre');
+for (const pre of pres) {
+  pre.textContent = pre.textContent.trimEnd();
 }
 
 const carousels = document.querySelectorAll('.carousel');
